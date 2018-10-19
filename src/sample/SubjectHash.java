12b48663c426221ff67ae6972d90df2e87d55b67
Dosl <best_dosl@outlook.co.th>
2018-10-19 16:05:40 +0700

    Change from best

---
+++ src/sample/SubjectHash.java
@@ -0,0 +1,14 @@
+package sample;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class SubjectHash {
+    ArrayList<String> subjectList = new ArrayList<>();
+//    HashMap<String, ArrayList> subjectMap = new HashMap<>(); //{01418114, Introduction to Computer Science}
+    public void checkSubject(){
+//        subjectMap.put("01418114",subjectList.add(0,"Introduction to Computer Science"));
+        subjectList.add("01418114");
+        subjectList.add("01418116");
+    }
+}
